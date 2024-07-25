67955c2180fff01e5afaae53af807907adc5cc63
bayucr <bayfore@gmail.com>
2024-07-25 15:22:29 +0700

    Selesai
    Sesuai video tutorial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,35 @@
-import { Slot } from "expo-router";
+import TimerProvider from "@/context/TimerContext";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+//this will prevent splash screen from autohide until loading all assets
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Slot />
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
+
+  return (
+    <TimerProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(modal)/adjust-meditation-duration"
+          options={{ headerShown: false, presentation: "modal" }}
+        />
+      </Stack>
+    </TimerProvider>
+  );
 }
